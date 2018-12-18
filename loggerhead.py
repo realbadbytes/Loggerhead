98097031512a9127a0a464d3758f4ec5f6e9ab15
realbadbytes <badbytes.io@gmail.com>
2018-12-18 12:50:47 -0800

    fixing api

--- loggerhead.py
+++ loggerhead.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
+# https://iextrading.com/developer/docs
 
-
-import intrinio_sdk
 import time
 import coloredlogs
 import verboselogs
 import logging
+from iexfinance.stocks import Stock
 from time import sleep
-from intrinio_sdk.rest import ApiException
 from industries import NYSE_HEALTHCARE
 
 
-intrinio_sdk.ApiClient().configuration.api_key['api_key'] = 'OjA3M2JmZjJkZWIxMDhjNTE0YTc5OTgwMDdlNGM5NDkx'
-company_api = intrinio_sdk.CompanyApi()
-#logger = logging.getLogger('Loggerhead')
 logger = verboselogs.VerboseLogger('Loggerhead')
+endpoint = 'https://api.iextrading.com/1.0'
+
 
 def get_data(symbol_list, industry_name):
     logger.info('Pulling data for industry: {}'.format(industry_name))
     for symbol in symbol_list:
-        try:
-            eps = company_api.get_company_data_point_number(symbol, tag='basiceps')
-            # Filter by companies that have positive EPS
-            if eps > 0:
-                sleep(1)
-                mktcap = company_api.get_company_data_point_number(symbol, tag='marketcap')
-
-                # We target mkt cap of $100 to 2 billion ($400 million later)
-                if 100000000 < mktcap < 2000000000:
-                    logger.success('Positive EPS and good market cap range for symbol {}'.format(symbol))
-                    # Get relevant metrics for hit
-                    sleep(1)
-                    lastprice = company_api.get_company_data_point_number(symbol, tag='last_price')
-                    sleep(1)
-                    multiple = company_api.get_company_data_point_number(symbol, tag='pricetoearnings')
-                    #evtoebitda = company_api.get_company_data_point_number(symbol, tag='evtoebitda')
-                    #epsgrowth = company_api.get_company_data_point_number(symbol, tag='epsgrowth')
-                    logger.success('{0}\nLast price: {1}\nMultiple: {2}\EV/EBITDA: {3}\nEPS Growth: {4}'.format(symbol, lastprice, evtoebitda, epsgrowth, multiple))
-
-        except ApiException as e:
-            logger.error('ApiException on {}: {}'.format(symbol, str(e)))
-            continue
+        stock = Stock(symbol)
+        key_stats = stock.get_key_stats()
+        name = key_stats['companyName']
+        print(name)
+        marketcap = key_stats['marketcap']
+        latest_eps = key_stats['latestEPS']
+        last_price = stock.get_price()
+        if (latest_eps > 0) and (100000000 < marketcap <= 2000000000):
+            multiple = (last_price / latest_eps)
+            logger.success('Hit on {0}:{1}\nLast price: {2}\nMultiple: {3}'.format(name, symbol, last_price, multiple))
 
 
 def main():
-    coloredlogs.install(level='DEBUG')
+    coloredlogs.install(level='INFO')
     coloredlogs.increase_verbosity()
     get_data(NYSE_HEALTHCARE, 'healthcare')
 
