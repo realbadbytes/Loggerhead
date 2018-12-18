a9e6b573d8e3da757847d5d4d356c5ef81e264c4
realbadbytes <badbytes.io@gmail.com>
2018-12-17 23:55:08 -0800

    filtering symbols successfully

--- industries.py
+++ industries.py
@@ -1,11 +1,17 @@
 # File to store stock symbol by industry
 
-HEALTHCARE = \
+NYSE_HEALTHCARE = \
         ['MMM', 'AAC', 'ABT', 'ABBV', 'AGN', 'ARA', 'ABC', 'ANTM',
         'RCUS', 'AZN', 'AVNS', 'BHC', 'BAX', 'BDX', 'BDXA', 'BHVN',
         'BSX', 'BMY', 'BKD', 'CBM', 'CMD', 'CSU', 'CAH', 'CTLT',
         'CNC', 'CRL', 'CHE', 'CI', 'CIVI', 'CYH', 'CCM', 'CRY',
         'CVS', 'DVA', 'DPLO', 'RDY', 'EW', 'ELAN', 'LLY', 'EBS',
         'EHC', 'ENZ', 'FMS', 'GEN', 'GKOS', 'GSK', 'GMED', 'HAE',
-        'HNGR', 'HCA']
+        'HNGR', 'HCA', 'HLF', 'HRC', 'HUM', 'INSP', 'IVC', 'NVTA',
+        'IQV', 'JNJ', 'KDMN', 'LH', 'LCI', 'MNK', 'MCK', 'MD', 'MDT',
+        'MRK', 'MOH', 'MSA', 'MYOV', 'NVRO', 'NVS', 'NVO', 'NUS', 'OMI',
+        'PEN', 'PRGO', 'PFE', 'PBH', 'QGEN', 'DGX', 'QHC', 'RMED',
+        'RMD', 'RAD', 'SNY', 'SEM', 'SNN', 'STE', 'SYK', 'TARO', 'TDOC',
+        'TFX', 'THC', 'TEVA', 'COO', 'USPH', 'UNH', 'UHS', 'VAPO',
+        'VAR', 'WCG', 'ZBH', 'ZTS', 'ZYME']
 
--- loggerhead.py
+++ loggerhead.py
@@ -6,7 +6,7 @@ import intrinio_sdk
 import time
 from intrinio_sdk.rest import ApiException
 from pprint import pprint
-from industries import HEALTHCARE
+from industries import NYSE_HEALTHCARE
 
 
 intrinio_sdk.ApiClient().configuration.api_key['api_key'] = 'OjA3M2JmZjJkZWIxMDhjNTE0YTc5OTgwMDdlNGM5NDkx'
@@ -15,18 +15,29 @@ company_api = intrinio_sdk.CompanyApi()
 
 def get_data(industry_list):
     for symbol in industry_list:
+        # Filter by companies that have positive EPS
         try:
-            #mktcap = company_api.get_company_data_point_number(symbol, tag='marketcap')
-            #lastprice = company_api.get_company_data_point_number(symbol, tag='last_price')
-            multiple = company_api.get_company_data_point_number(symbol, tag='pricetoearnings')
-            pprint('{} @ multiple {}'.format(symbol, multiple))
+            eps = company_api.get_company_data_point_number(symbol, tag='basiceps')
+            if eps > 0:
+                #print('Positive EPS on {}'.format(symbol))
+                mktcap = company_api.get_company_data_point_number(symbol, tag='marketcap')
+
+                # We target mkt cap of $100 to $400 million
+                if 100000000 < mktcap < 2000000000:
+                    print('Positive EPS and target mkt cap range on {}'.format(symbol))
+                    #lastprice = company_api.get_company_data_point_number(symbol, tag='last_price')
+                    multiple = company_api.get_company_data_point_number(symbol, tag='pricetoearnings')
+                    #evtoebitda = company_api.get_company_data_point_number(symbol, tag='evtoebitda')
+                    epsgrowth = company_api.get_company_data_point_number(symbol, tag='epsgrowth')
+                    print('{} @ multiple {}'.format(symbol, multiple))
+
         except ApiException as e:
-            print('ApiException on {}'.format(symbol))
+            #print('ApiException on {}'.format(symbol))
             continue
 
 
 def main():
-    get_data(HEALTHCARE)
+    get_data(NYSE_HEALTHCARE)
 
 
 if __name__ == '__main__':
