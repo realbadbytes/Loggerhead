1c4fe8dea45b7f42d165db5397dca8218b18d835
realbadbytes <badbytes.io@gmail.com>
2018-12-18 13:11:41 -0800

    usage function

--- loggerhead.py
+++ loggerhead.py
@@ -28,6 +28,8 @@ def process_symbols(symbol_list, industry_name):
         marketcap = key_stats['marketcap']
         latest_eps = key_stats['latestEPS']
         last_price = stock.get_price()
+        # Filter by cramer's recommended "improperly handicapped" zone of $100 to $400 million
+        # Widen to $2 billion for now because hits are very low.
         if (latest_eps > 0) and (100000000 < marketcap <= 2000000000):
             multiple = (last_price / latest_eps)
             container['symbol'] = symbol
@@ -39,12 +41,23 @@ def process_symbols(symbol_list, industry_name):
     return hits
 
 
+def usage():
+    print('Loggerhead v1.0\n\n\tusage: ./loggerhead.py [industry]\n\n\tAvailable industries:\n' \
+            '\n\tbasic\tcapital_goods\tconsumer_durables\tconsumer_nondurables\n' \
+            '\tconsumer_services\ttenergy\tfinance\thealthcare\n' \
+            '\tmisc\tpublic_utilities\ttechnology\ttransportation\n')
+
+
 def main():
+    if len(sys.argv) != 2:
+        usage()
+        exit(0)
+
     industry = sys.argv[1]
     coloredlogs.install(level='INFO')
     coloredlogs.increase_verbosity()
     hits = process_symbols(NYSE_HEALTHCARE, industry)
-    logger.success('Found {0} stocks of interest in {1} sector.'.format(len(hits), industry))
+    logger.success('Found {0} stock(s) of interest in the {1} industry.'.format(len(hits), industry))
 
 
 if __name__ == '__main__':
